83e45ceb0c2d91647400ba02b00b80bff129f374
imran-rassi <imran.rassi.2020@gmail.com>
2023-01-24 22:29:18 -0500

    added the latest updates

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,70 +3,79 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// * TODO: Add and configure workbox plugins for a service worker and manifest file.
-// * Add CSS loaders and babel to webpack.
-
 module.exports = () => {
-	return {
-		mode: 'development',
-		entry: {
-			main: './src/js/index.js',
-			install: './src/js/install.js',
-		},
-		output: {
-			filename: '[name].bundle.js',
-			path: path.resolve(__dirname, 'dist'),
-		},
-		plugins: [
-			new HtmlWebpackPlugin({
-				template: './index.html',
-			}),
-			new WebpackPwaManifest({
-				name: 'JATE',
-				short_name: 'JATE',
-				description: 'Just Another Text Editor',
-				display: 'standalone',
-				background_color: '#1e1e1e',
-				theme_color: '#1e1e1e',
-				start_url: '/',
-				publicPath: '/',
-				fingerprints: false,
-				inject: true,
-				icons: [
-					{
-						src: path.resolve('src/images/logo.png'),
-						sizes: [96, 128, 192, 256, 384, 512],
-						destination: path.join('assets', 'icons'),
-					},
-				],
-			}),
-			new InjectManifest({
-				swSrc: './src-sw.js',
-				swDest: 'service-worker.js',
-			}),
-		],
+  return {
+    mode: 'development',
+    // Entry point for files
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    // Output for our bundles
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
 
-		module: {
-			rules: [
-				{
-					test: /\.css$/i,
-					use: ['style-loader', 'css-loader'],
-				},
-				{
-					test: /\.m?js$/,
-					exclude: /node_modules/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: ['@babel/preset-env'],
-							plugins: [
-								'@babel/plugin-proposal-object-rest-spread',
-								'@babel/transform-runtime',
-							],
-						},
-					},
-				},
-			],
-		},
-	};
+    // Add and configure workbox plugins for a service worker and manifest file.
+    plugins: [
+     // Webpack plugin that generates our html file and injects our bundles. 
+     new HtmlWebpackPlugin({
+      // Creates a copy of the index.html file in the dist folder and inserts in script tag to the newly created bundle.js file
+      template: './index.html', 
+      // Optional parameters
+      title: 'Text Editor'
+    }),
+   
+    // Injects our custom service worker
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }),
+
+    // Creates a manifest.json file.
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Just another text editor',
+      short_name: 'JATE',
+      description: 'Just another text editor!',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: '/',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+    ],
+
+    module: {
+      // Add CSS loaders to webpack
+      rules: [
+        {
+          // This looks for a .css file and adds this into the bundle.js file
+          test: /\.css$/i,
+          // Additional modules for converting the css into js
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Add babel-loader to webpack in order to use ES6
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"]
+            }
+          }
+        }
+      ],
+    },
+  };
 };
